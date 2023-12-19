9bae595a909033960bd5bfa3472655e9cfc56c9c
Aitsimh Nizar <aitsimh.nizar@emsi-edu.ma>
2023-12-19 16:47:47 +0100

    1.0.0

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'frontend-api';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+  
+}
