ea71fdcd929085fe3a1c96ceccd7b4574274b138
Aitsimh Nizar <aitsimh.nizar@emsi-edu.ma>
2024-01-03 16:44:44 +0100

    ThemeService

--- src/app/components/widgets/login/login.component.ts
+++ src/app/components/widgets/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { ThemeService } from '../../../theme.service';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  constructor(private themeService: ThemeService) {}
 
+  ngOnInit(): void {
+    // On page load or when changing themes
+    this.themeService.toggleTheme();
+
+    // You might want to add an event listener to listen for changes in the future
+    // For example, if the user changes the theme in settings
+    // window.addEventListener('themeChanged', this.themeService.toggleTheme);
+  }
 }
