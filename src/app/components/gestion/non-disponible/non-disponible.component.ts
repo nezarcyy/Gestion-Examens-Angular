e399825bf88fe6af966268c2d8f7d0c2bd5f3e16
Aitsimh Nizar <aitsimh.nizar@emsi-edu.ma>
2024-01-04 22:43:38 +0100

    NonDispManagementService

--- src/app/components/gestion/non-disponible/non-disponible.component.ts
+++ src/app/components/gestion/non-disponible/non-disponible.component.ts
@@ -1,12 +1,85 @@
 import { Component } from '@angular/core';
+import { NonDesponibilitie } from '../../../models/nonDisponibilites.models';
+
 
 @Component({
   selector: 'app-non-disponible',
-  standalone: true,
-  imports: [],
   templateUrl: './non-disponible.component.html',
-  styleUrl: './non-disponible.component.css'
+  styleUrls: ['./non-disponible.component.css']
 })
 export class NonDisponibleComponent {
 
+  nonDispos: NonDesponibilitie[] = [
+    {
+      id: 1,
+      jour: 'Lundi',
+      enseignant: {
+        id: 1, nom: 'Ahmed', prenom: 'Ahmed',
+        civilite: '',
+        tel: '',
+        cne: '',
+        email: '',
+        login: '',
+        password: '',
+        specialite: ''
+      },
+      periode: '8h-10h'
+    },
+    {
+      id: 2,
+      jour: 'Mardi',
+       enseignant: {
+        id: 1, nom: 'Ahmed', prenom: 'Ahmed',
+        civilite: '',
+        tel: '',
+        cne: '',
+        email: '',
+        login: '',
+        password: '',
+        specialite: ''
+      },
+      periode: '8h-10h'
+    },
+    {
+      id: 3,
+      jour: 'Mercredi',
+      enseignant: {
+        id: 1, nom: 'Ahmed', prenom: 'Ahmed',
+        civilite: '',
+        tel: '',
+        cne: '',
+        email: '',
+        login: '',
+        password: '',
+        specialite: ''
+      },
+      periode: '8h-10h'
+    },
+    {
+      id: 4,
+      jour: 'Jeudi',
+      enseignant: {
+        id: 1, nom: 'Ahmed', prenom: 'Ahmed',
+        civilite: '',
+        tel: '',
+        cne: '',
+        email: '',
+        login: '',
+        password: '',
+        specialite: ''
+      },
+      periode: '8h-10h'
+    },
+  ];
+
+   gotoPage(page: number): void {
+   
+  }
+
+  goToPreviousSet(): void {
+   
+  }
+
+  goToNextSet(): void {
+   }
 }
