bee3fff9d27255ad22b09e304103ac0eae97d47c
Aitsimh Nizar <aitsimh.nizar@emsi-edu.ma>
2024-01-04 22:09:15 +0100

    Services2.0

---
+++ src/app/services/non-desponibilitie.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class NonDesponibilitieService {
+  searchNonDispo(keyword: any, page: number, size: number) {
+    throw new Error('Method not implemented.');
+  }
+
+}
