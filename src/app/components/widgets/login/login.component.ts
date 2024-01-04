22c3910a01cb5fc83b80834f2060a06cc3ddc083
Aitsimh Nizar <aitsimh.nizar@emsi-edu.ma>
2024-01-04 21:21:27 +0100

    LoginTS

--- src/app/components/widgets/login/login.component.ts
+++ src/app/components/widgets/login/login.component.ts
@@ -1,23 +1,82 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from '../../../services/auth.service';
 import { ThemeService } from '../../../theme.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor(private themeService: ThemeService) {}
+  errorMessage!: string;
+  formGroup!: FormGroup;
 
-  ngOnInit(): void {
-    // On page load or when changing themes
+  constructor(
+    private themeService: ThemeService,
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private router: Router,
+    private cookieService: CookieService
+  ) {}
+
+  ngOnInit() {
+
     this.themeService.toggleTheme();
+    this.formGroup = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
-    // You might want to add an event listener to listen for changes in the future
-    // For example, if the user changes the theme in settings
-    // window.addEventListener('themeChanged', this.themeService.toggleTheme);
+  get username() {
+    return this.formGroup.get('username');
+  }
+
+  get password() {
+    return this.formGroup.get('password');
+  }
+
+  login() {
+    if (this.formGroup.invalid) {
+      // Mark form controls as touched to display validation errors
+      this.formGroup.markAllAsTouched();
+      return;
+    }
+
+    const username = this.username?.value;
+    const password = this.password?.value;
+
+    this.authService.login(username, password).subscribe(response => {
+      if(response.authenticated==true){
+        console.log(response);
+
+        this.authService.loggedIn = response.authenticated;
+        this.authService.isAdmin = response.admin;
+        this.authService.isProf = response.enseignant;
+        this.authService.name = response.nom + " " + response.prenom;
+        this.authService.token = response.token;
+        this.authService.id = response.id;
+
+
+        this.cookieService.set('username', this.authService.name);
+        this.cookieService.set('userId', this.authService.id.toString());
+        let role = response.admin ? 'Administrateur' : 'Ensignant';
+        this.cookieService.set('role', role);
+        // refresh page
+        window.location.reload();
+        this.router.navigateByUrl('/home');
+
+      }else {
+        Swal.fire('Echec', 'Nom d\'utilisateur ou mot de passe incorrect', 'error');
+
+      }
+        },
+        error => {
+           Swal.fire('Echec', 'Nom d\'utilisateur ou mot de passe incorrect', 'error');});
+
   }
 }
