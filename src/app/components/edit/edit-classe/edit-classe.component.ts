b13044233430219b95e42586ae84d1202860c231
Aitsimh Nizar <aitsimh.nizar@emsi-edu.ma>
2024-01-04 22:54:05 +0100

    ClasseEdit

--- src/app/components/edit/edit-classe/edit-classe.component.ts
+++ src/app/components/edit/edit-classe/edit-classe.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import Swal from "sweetalert2";
+import {ClasseService} from "../../../services/classe.service";
+import {Classe} from "../../../models/classes.models";
 
 @Component({
   selector: 'app-edit-classe',
-  standalone: true,
-  imports: [],
   templateUrl: './edit-classe.component.html',
-  styleUrl: './edit-classe.component.css'
+  styleUrls: ['./edit-classe.component.css']
 })
-export class EditClasseComponent {
+export class EditClasseComponent implements OnInit {
+  editClasseFormGroup!: FormGroup;
+  classe!: Classe;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
+    private clService: ClasseService
+  ) {
+    this.classe = this.router.getCurrentNavigation()?.extras.state as Classe;
+  }
+
+  ngOnInit(): void {
+    this.editClasseFormGroup = this.fb.group({
+      libelle: [''],
+      nbrEleves: [''],
+      filiere: ['']
+    });
+    this.setFormValues();
+  }
+  setFormValues() {
+    if (this.classe) {
+      this.editClasseFormGroup.patchValue({
+        libelle: this.classe.libelle,
+        nbrEleves: this.classe.nbrEleves,
+        filiere: this.classe.filiere.libelle
+      });
+    }
+  }
+  handleUpdateClasse() {
+
+    if (this.editClasseFormGroup.valid && this.classe) {
+      const updatedClasse: Classe = {
+        ...this.classe,
+        ...this.editClasseFormGroup.value
+      };
+      this.clService.updateClasse(updatedClasse.id,updatedClasse).subscribe((data) => {
+          Swal.fire( 'Succès', 'Classe modifié avec succès','success');
+          this.router.navigateByUrl('/classes');
+        }
+
+      );
+
+    }
+  }
 
 }
