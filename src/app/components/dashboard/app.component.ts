64ebd4db0d6b40e00c81d0b3e340792d76b7a53a
Aitsimh Nizar <aitsimh.nizar@emsi-edu.ma>
2024-01-04 22:44:52 +0100

    Dashboard

--- src/app/components/dashboard/app.component.ts
+++ src/app/components/dashboard/app.component.ts
@@ -1,1 +1,20 @@
-while
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'Gestion Emploi du temps';
+  isAuthenticated!: boolean;
+
+  constructor(private cookieService: CookieService) {
+  }
+
+  ngOnInit() {
+    this.isAuthenticated = this.cookieService.check('username');
+  }
+}
