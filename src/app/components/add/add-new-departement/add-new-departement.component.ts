f0781288eebce52223add6968942762c27999255
Aitsimh Nizar <aitsimh.nizar@emsi-edu.ma>
2024-01-05 23:02:08 +0100

    AddDepartement

--- src/app/components/add/add-new-departement/add-new-departement.component.ts
+++ src/app/components/add/add-new-departement/add-new-departement.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { DepartmentService } from '../../../services/department.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Departement } from '../../../models/departement.models';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-new-departement',
-  standalone: true,
-  imports: [],
   templateUrl: './add-new-departement.component.html',
-  styleUrl: './add-new-departement.component.css'
+  styleUrls: ['./add-new-departement.component.css']
 })
-export class AddNewDepartementComponent {
+export class AddNewDepartementComponent implements OnInit {
+  newDepartementFormGroup!: FormGroup;
 
+  constructor(private fb: FormBuilder,private dpService:DepartmentService,
+    private router: Router) {}
+
+  ngOnInit(): void {
+    this.newDepartementFormGroup = this.fb.group({
+      libelle: this.fb.control(null, [Validators.required]),
+      chefDepartement: this.fb.control(null, [Validators.required])
+    });
+  }
+
+  handleAddDepartement() {
+    if (this.newDepartementFormGroup.valid) {
+    const newDepart: Departement = this.newDepartementFormGroup.value;
+    this.dpService.saveDepartment(newDepart).subscribe({
+      next: data => {
+        Swal.fire('Succès', 'Département ajouté avec succès', 'success');
+        this.router.navigateByUrl('/departements');
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  } else {
+    Swal.fire('Erreur', 'Veuillez remplir correctement tous les champs du formulaire', 'error');
+  }
+  }
 }
